d8f6dddaf1eedf34c6dbd02df60f39f79b898500
semaxio <kutchms@gmail.com>
2025-10-27 16:44:34 +0300

    UPDATE branch for PR

--- src/app/[lng]/Content.tsx
+++ src/app/[lng]/Content.tsx
@@ -95,7 +95,6 @@ export function Content({ children }: { children: React.ReactNode }) {
       <Header onLanguageChange={onLanguageChange} language={currentLocale} isAuth={isAuth}>
         <NotificationsDropdown />
       </Header>
-
       <div className='max-w-[1920px]'>
         <div className='flex'>
           {isAuth && data && (
